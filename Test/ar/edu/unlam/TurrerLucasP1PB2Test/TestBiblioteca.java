927d86ed7a759d048716473b8a04ddd019e5366a
Lucas-Turrer <63521780+Lucas-Turrer@users.noreply.github.com>
2021-06-01 20:37:29 -0300

    segundo commit

--- Test/ar/edu/unlam/TurrerLucasP1PB2Test/TestBiblioteca.java
+++ Test/ar/edu/unlam/TurrerLucasP1PB2Test/TestBiblioteca.java
@@ -4,11 +4,41 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ar.edu.unlam.TurrerLucasP1PB2.Estudiante;
+import ar.edu.unlam.TurrerLucasP1PB2.Geografia;
+import ar.edu.unlam.TurrerLucasP1PB2.Historia;
+
 public class TestBiblioteca {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void QueUnEstudiantePideDosLibros() {
+		Estudiante alumno = new Estudiante("Lucas", "Turrer", 37931874);
+		Historia libro1 = new Historia(001,"Los Andes", "San Martin");
+		Geografia libro2 = new Geografia(002, "Egipto", "Eduar");
+		
+		alumno.pedirLibro(libro1);
+		alumno.pedirLibro(libro2);
+		
+		Integer valorEsperado=2;
+		Integer valorObtenido=alumno.librosEnSuPoder();
+		
+		assertEquals(valorEsperado, valorObtenido);
+	}
+	
+	@Test
+	public void QueUnEstudianteNoPuedePedirMasDeDosLibros() {
+		Estudiante alumno = new Estudiante("Lucas", "Turrer", 37931874);
+		Historia libro1 = new Historia(001,"Los Andes", "San Martin");
+		Geografia libro2 = new Geografia(002, "Egipto", "Eduar");
+		
+		alumno.pedirLibro(libro1);
+		alumno.pedirLibro(libro2);
+		
+		Boolean valorObtenido=alumno.seguirRetirando();
+		System.out.println(valorObtenido);
+		assertFalse(valorObtenido);
+		
+	
 	}
 
 }
--- src/ar/edu/unlam/TurrerLucasP1PB2/Estudiante.java
+++ src/ar/edu/unlam/TurrerLucasP1PB2/Estudiante.java
@@ -1,13 +1,32 @@
 package ar.edu.unlam.TurrerLucasP1PB2;
 
+import java.util.ArrayList;
+
 public class Estudiante {
 	protected String nombre;
 	protected String apellido;
 	protected Integer dni;
+	protected ArrayList<Libro> librosPedidos = new ArrayList<Libro>();
+
 	
 	public Estudiante (String nombre, String apellido, Integer dni) {
 		this.nombre=nombre;
 		this.apellido=apellido;
 		this.dni=dni;
+	}
+
+	public void pedirLibro(Libro libro) {
+		librosPedidos.add(libro);
+	}
+
+	public Integer librosEnSuPoder() {
+		return librosPedidos.size();
+	}
+
+	public Boolean seguirRetirando() {
+		if(librosPedidos.size()==2) {
+			return false;
+		}
+		return true;
 	}
 }
